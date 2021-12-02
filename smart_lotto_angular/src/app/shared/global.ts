09a826bfe71caf5571f322b9a3b98431914368e1
sreeparnadas <sreeparnadas0675@gmail.com>
2021-12-02 15:57:56 +0530

    url updated

--- smart_lotto_angular/src/app/shared/global.ts
+++ smart_lotto_angular/src/app/shared/global.ts
@@ -6,7 +6,7 @@ export const GlobalVariable = Object.freeze({
   // BASE_API_URL: 'http://127.0.0.1:8000/api',
   // BASE_API_URL_DEV: 'http://127.0.0.1:8000/api/dev',
 
-  BASE_API_URL: 'https://my.goarajshree.com/smart_lotto_api/public/api',
-  BASE_API_URL_DEV: 'https://my.goarajshree.com/smart_lotto_api/public/api/dev',
+  BASE_API_URL: 'https://bhagyadata.com/smart_lotto_api/public/api',
+  BASE_API_URL_DEV: 'https://bhagyadata.com/smart_lotto_api/public/api/dev',
   // ... more of your variables
 });
