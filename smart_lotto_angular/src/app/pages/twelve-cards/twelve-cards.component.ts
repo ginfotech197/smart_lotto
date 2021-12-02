769dd16657377e85ac208e2b04e32bfd98a10050
priyamghosh1 <priyamghosh73@gmail.com>
2021-12-02 21:55:03 +0530

    updated

--- smart_lotto_angular/src/app/pages/twelve-cards/twelve-cards.component.ts
+++ smart_lotto_angular/src/app/pages/twelve-cards/twelve-cards.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CommonService} from '../../services/common.service';
+import {DrawTime} from '../../models/DrawTime.model';
+import {User} from '../../models/user.model';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-twelve-cards',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./twelve-cards.component.scss']
 })
 export class TwelveCardsComponent implements OnInit {
+  activeDrawTime: DrawTime;
+  user: User;
 
-  constructor() { }
+
+
+  constructor(private commonService: CommonService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    this.authService.userBehaviorSubject.subscribe(user => {
+      this.user = user;
+    });
+
+    this.activeDrawTime = this.commonService.getActiveDrawTime();
+    this.commonService.getActiveDrawTimeListener().subscribe((response: DrawTime) => {
+      this.activeDrawTime = response;
+    });
+
+  }
+
+  detailsData(value: string){
+    console.log(value);
+  }
+
+  saveUserInput(){
+    const playMaster = {
+      drawMasterId: this.activeDrawTime.drawId,
+      userId: this.user.userId,
+    };
+
+    console.log(playMaster);
   }
 
 }
