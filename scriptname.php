4cc78a6c4c63df760e8e1e196bf29d80657472ae
priyamghosh1 <priyamghosh73@gmail.com>
2021-12-06 23:31:45 +0530

    sheduler updated

--- scriptname.php
+++ scriptname.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = 'https://kfatafat.com/matka_api/public/api/dev/createAutoResult';
+$url = 'https://bhagyadata.com/matka_api/public/api/dev/createAutoResult';
 	$data = array();
  
 	// use key 'http' even if you send the request to https://...
